94ab961f79d4b3342b45a1234988a1fa797950d9
Nataly Ruiz <NRuiz51@cps.edu>
2024-04-26 08:04:26 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ruiz, Nataly on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
